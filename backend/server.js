8470bf56d07c9fe65ca758a6db41c7040675b528
Anmol2005 <asharma91975@gmail.com>
2026-08-31 16:54:00 +0530

    Fix server port configuration

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = require("./backendManagement/app");
 const connectDB = require("./backendManagement/config/dbConnect");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 connectDB();
 
 app.listen(port, () => {
